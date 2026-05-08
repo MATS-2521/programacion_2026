78f8ff7073c1b0ed34710cb1dd324bbd7cb6d5e4
Marco Antonio Trejo Sulub <a25216368@alumnos.uady.mx>
2026-05-08 12:52:30 -0600

    ejercicio de la clase

--- Hello_World/Hello_World.cpp
+++
@@ -1,9 +0,0 @@
-//Hello_World
-#include <iostream>
-
-int main()
-{
-    std::cout<<"Hello World";
-
-    return 0;
-}
--- retos/reto_02/Mensaje.cpp
+++
@@ -1,7 +0,0 @@
-#include "mensaje.h"
-
-Mensaje::Mensaje(const std::string& texto, const std::string& autor){
-    contenido = texto;
-    remitente = autor;
-    FechaHora = std::time(nullptr);
-}
